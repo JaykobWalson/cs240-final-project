d09274fd5f70defcde22f6c221d2aa9a7c79e099
Jaykob Walson <jwalson@pugetsound.edu>
2021-11-21 16:57:30 -0800

    Added tron-style background with skrillex. A custom projectile is in the WIP.

--- app.js
+++ app.js
@@ -1,1 +1,15 @@
 var game = new Game();
+
+let blastMusic = document.getElementById("music");
+blastMusic.addEventListener("click", function () {
+  let music = new Audio("skrillex.mp3");
+  music.play();
+});
+
+function drawShape() {
+  var c = document.getElementById("myCanvas");
+  var ctx = c.getContext("2d");
+  ctx.beginPath();
+  ctx.arc(100, 75, 50, 0, 2 * Math.PI);
+  ctx.stroke();
+}
--- game.js
+++ game.js
@@ -1,15 +1,15 @@
-class Game{
-    constructor(){
-        this.engine = Matter.Engine.create();
-        this.renderer = Matter.Render.create({
-            element: document.body,
-            engine: this.engine,
-            options:{
-                showDebug: true,
-            }
-        });
+class Game {
+  constructor() {
+    this.engine = Matter.Engine.create();
+    this.renderer = Matter.Render.create({
+      element: document.body,
+      engine: this.engine,
+      options: {
+        showDebug: true,
+      },
+    });
 
-        Matter.Render.run(this.renderer);
-        Matter.Runner.run(this.engine);
-    }
+    Matter.Render.run(this.renderer);
+    Matter.Runner.run(this.engine);
+  }
 }
