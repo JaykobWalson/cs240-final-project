4f7d7fae0f8570c07e08c778111fdacca937e53d
Jaykob Walson <jwalson@pugetsound.edu>
2021-12-06 17:05:06 -0800

    Added a test button to switch backgrounds. Still trying to get it to switch back to the initial round after every 5 rounds (ex. round 6, round 11, etc.)

--- app.js
+++ app.js
@@ -1,11 +1,12 @@
 var game = new Game();
 var music = new Audio("Shiny Tech III.mp3");
 var musicPlaying = false;
+var stage = 1;
 var round = parseInt(document.getElementById("level").innerHTML);
 let blastMusic = document.getElementById("music");
 var colors = [];
 var currentColor = document.getElementsByClassName("glowstage" + round);
-listOfColors();
+// listOfColors();
 blastMusic.addEventListener("click", function () {
   music.play();
   musicPlaying = true;
@@ -13,18 +14,46 @@ blastMusic.addEventListener("click", function () {
 
 //Sets up the list of glowing colors that switch every round until the 5th round.
 //Beyond the 5th * n round, the background color will go back to glowstage1.
-function listOfColors() {
-  for (let i = 0; i < 5; i++) {
-    let colorToAdd = document.getElementsByClassName("glowstage" + (i + 1));
-    colors[i] = colorToAdd;
-    console.log(colors[i]);
-  }
-}
+// function listOfColors() {
+//   for (let i = 0; i < 5; i++) {
+//     let colorToAdd = document.getElementsByClassName("glowstage" + (i + 1));
+//     colors[i] = colorToAdd;
+//     console.log(colors[i]);
+//   }
+// }
 
 let advanceRounds = document.getElementById("nextRound");
 advanceRounds.addEventListener("click", function () {
   round++;
   document.getElementById("level").innerHTML = round;
+  document.getElementById("level").classList.add("glowstage" + stage);
+  document.getElementById("title").classList.add("glowstage" + stage);
+  this.classList.clear;
+  document.getElementById("colorOfRounds").classList.add("glowstage" + stage);
+  console.log(stage);
+  if (true) {
+    document.getElementById("level").innerHTML = round;
+    document.getElementById("level").classList.add("glowstage" + stage);
+    document.getElementById("title").classList.add("glowstage" + stage);
+    document.getElementById("colorOfRounds").classList.add("glowstage" + stage);
+    this.classList.clear;
+    console.log(stage);
+    stage++;
+    if (stage > 5) {
+      stage = 1;
+    }
+  }
+  //if (remainder == 1) {
+  // document.getElementById("level").innerHTML = round;
+  // document.getElementById("level").classList.add("glowstage" + this.innerHTML);
+  // document.getElementById("title").classList.add("glowstage" + stage);
+  // document.getElementById("colorOfRounds").classList.add("glowstage" + stage);
+  // console.log(stage);
+  // stage++;
+  // if (stage > 5) {
+  //   stage = 1;
+  // }
+  //}
 });
 
 let resetMusic = document.getElementById("music");
